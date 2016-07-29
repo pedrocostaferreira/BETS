67b4d7e48628e6f977d8095fe1f7b0e3f1993b77
talithafs <talitha.speranza@gmail.com>
2016-07-29 13:43:39 -0300

    Adicionada assinatura da BETS.plot

---
+++ R/BETS.plot.R
@@ -0,0 +1,19 @@
+#' @title  xxx
+#' 
+#' @description  xxxx
+#' 
+#' @param ts xxx
+#' @param lag.max xxx
+#' @param mode xxx
+#' @param ci xxx 
+#' 
+#' @return xxx
+#' 
+#' @author Talitha Speranza \email{talitha.speranza@fgv.br}
+#' 
+#' @export
+
+
+BETS.plot = function(ts, start = NULL, end = NULL, ylab = NULL, type = "series"){
+  
+}
