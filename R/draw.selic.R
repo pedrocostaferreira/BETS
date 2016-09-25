b92d5a4249366e2cbaeff83e80a4dfbd51b598df
talithafs <talitha.speranza@gmail.com>
2016-09-25 01:29:47 -0300

    Grafico da SELIC modificado

--- R/draw.selic.R
+++ R/draw.selic.R
@@ -27,26 +27,54 @@ draw.selic = function(file, start = NULL, ylim = NULL, open = TRUE){
   
   selic = BETS.get(4189)
   
-  # --- QUE COMPLICACAO
-  #target = read.csv2("inst//target_selic.csv", stringsAsFactors = F)
-  #d = as.Date("2000-09-25")
-  #z <- zooreg(target[,2], start = d, frequency = 1)
-  #window(z, start = as.Date("2006-01-01"))
-  # OU subset(z, time %in% c(as.Date("2016-09-21"),as.Date("2016-09-22")))
-  # usar Sys.Date()
-  
   if(!is.null(start)){
     selic = window(selic, start = start)
   }
-  
-  if(is.null(ylim)){
-    ylim = c(min(selic)-1,max(selic)+1)
+  else{
+    start = start(selic)
   }
   
   dt = as.Date(selic)[length(selic)]
   last = vector(mode = "numeric")
   last[1] = as.integer(format(dt, "%Y"))
   last[2] = as.integer(format(dt, "%m"))
+  
+  # --- QUE COMPLICACAO
+  target = read.csv2("inst//target_selic.csv", stringsAsFactors = F)
+  
+  dates = vector(mode = "character")
+  curr = start
+  j = 1
+  
+  while(curr[1] != (last[1]+1)){
+    
+    for(i in curr[2]:12){
+      
+      month = i
+      
+      if(i < 10){
+        month = paste0("0",i)
+      }
+      
+      dates[j] = paste0(curr[1],"-",month,"-15")
+      j = j + 1
+    }
+    
+    curr[1] = curr[1] + 1
+    curr[2] = 1
+  }
+  
+  dates = as.Date(dates)
+  
+  zr = zooreg(target[,2], start = as.Date("2000-09-25"))
+  zr.sub = subset(zr, time %in% dates)
+  target = ts(zr.sub, start = start, frequency = 12)
+  target = window(target, end = last)
+  
+  if(is.null(ylim)){
+    ylim = c(min(selic)-2,max(selic)+2)
+  }
+  
   aval = paste0("Last available data: ",format(dt, "%b"),"/", format(dt,"%Y"))
   
   x.spam = last[1] - start[1]
@@ -54,7 +82,7 @@ draw.selic = function(file, start = NULL, ylim = NULL, open = TRUE){
   
   par(font.lab = 2, cex.axis = 1.2, bty = "n", las = 1)
   plot(selic, lwd = 2.5, lty = 1, xlab = "", ylab = "", main = "Base Interest Rate (SELIC)", col = "darkolivegreen", ylim = ylim)
-  #lines(trend, lty = 6, col = "darkgray", lwd = 2)
+  lines(target, lty = 6, col = "darkgray", lwd = 1)
   mtext("Accumulated in the Month, in Annual Terms")
   
   end.x = last[1]
@@ -62,7 +90,7 @@ draw.selic = function(file, start = NULL, ylim = NULL, open = TRUE){
   val = round(selic[length(selic)],2)
   
   points(end.x + d.x, val, pch = 21, cex = 1.25, lwd = 2, bg = "darkolivegreen", col = "darkgray")
-  text(start[1] + 0.14*x.spam, ylim[2] - 0.05*y.spam, aval, cex = 0.9)
+  text(start[1] + 0.14*x.spam, ylim[2] - 0.07*y.spam, aval, cex = 0.85)
   
   x1 = end.x + d.x 
   y0 = ylim[1] + 0.2*y.spam
@@ -70,6 +98,7 @@ draw.selic = function(file, start = NULL, ylim = NULL, open = TRUE){
   
   arrows(x0 = x1, x1 = x1, y0 = y0, y1 = y1, length = c(0.01*x.spam, 0.00006*y.spam), lwd = 2)
   text(x1 - 0.005*x.spam, y0 - 0.067*y.spam, as.character(val), cex = 1.1, font = 2)
+  legend("topleft", "Target (15th of the Month)", lty = 6, lwd = 1, col="darkgrey", bty = "n", cex = 0.85)
   
   add.notes(selic, ylim = ylim, xlim = c(start[1],last[1]))
   
