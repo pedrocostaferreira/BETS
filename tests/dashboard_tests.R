137ca8991779e13edc067215686a217190f58085
jonatha azevedo <johnmasterchip@gmail.com>
2016-11-03 16:31:38 -0200

    Delete dashboard_tests.R

--- tests/dashboard_tests.R
+++
@@ -1,8 +0,0 @@
-parameters = vector(mode = "list")
-
-parameters$author = "FGV/IBRE"
-parameters$url = "http://portalibre.fgv.br/"
-parameters$logo = "logo_ibre.png"
-parameters$text = "text.txt"
-
-BETS.dashboard(type = "inflation", parameters = parameters)
