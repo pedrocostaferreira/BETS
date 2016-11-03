ef0593dc379f150b6ec3934bc5163eb176f13bbf
jonatha azevedo <johnmasterchip@gmail.com>
2016-11-03 16:34:40 -0200

    Update get.data.frame.R

--- R/get.data.frame.R
+++ R/get.data.frame.R
@@ -1,6 +1,6 @@
 #' @title Get a BETS series as a data.frame.
 #' 
-#' @description By default, \code{\link{BETSget}} returns a \code{\link[stats]{ts}} object. However, there are many situations in which is more convenient to work with a data.frame. So, \code{get.data.frame} receives the code of a BETS series and returns a \code{\link[base]{data.frame}} containing the data of the corresponding series. Alternatively, a \code{ts} can be supplied, in which case the BETS databases will not be searched. 
+#' @description By default, \code{\link{BETS.get}} returns a \code{\link[stats]{ts}} object. However, there are many situations in which is more convenient to work with a data.frame. So, \code{get.data.frame} receives the code of a BETS series and returns a \code{\link[base]{data.frame}} containing the data of the corresponding series. Alternatively, a \code{ts} can be supplied, in which case the BETS databases will not be searched. 
 #' 
 #' @param code An \code{integer}. The unique identifier of the series within the BETS database.
 #' @param ts An \code{ts} object. A time series to be formatted as a data.frame. 
