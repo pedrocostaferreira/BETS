c394e2fda5bce5d776a45966c74eacbf31088cd9
jonatha azevedo <johnmasterchip@gmail.com>
2016-11-08 11:53:14 -0200

    Detalhes

--- R/BETS.t_test.R
+++ R/BETS.t_test.R
@@ -16,7 +16,7 @@
 #'     seasonal = c(1,1,1), method ="ML",lambda=0)
 #'     summary(fit.air)
 #' 
-#' significance test for model SARIMA(1,1,1)(1,1,1)_12
+#' # significance test for model SARIMA(1,1,1)(1,1,1)_12
 #' t.test(arima_model = fit.air)
 #'
 #' 
