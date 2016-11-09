c535c70aecbeea1e6670f27e222e6362149e4ae8
jonatha azevedo <johnmasterchip@gmail.com>
2016-11-09 19:16:10 -0200

    Add files via upload

---
+++ R/zzz.R
@@ -0,0 +1,23 @@
+.onLoad <- function(lib, pkg)
+{
+  base::suppressMessages(library("mFilter"))
+  
+  mylib <- base::dirname(system.file(package = "BETS"))
+  ver <- utils::packageDescription("BETS", lib = mylib)["Version"]
+  txt <- c("\n",
+           paste(sQuote("BETS"), "version:", ver),
+           "\n",
+           paste(sQuote("BETS"),
+                 "Brazilian Economic Time Series"),
+           "\n",
+           paste("See",
+                 sQuote("library(help=\"BETS\")"),
+                 "for details"),
+           "\n",
+           paste("BugReports: https://github.com/pedrocostaferreira/BETS/issues"),
+           "\n"
+  )
+  if(interactive() || getOption("verbose"))
+    writeLines(strwrap(txt, indent = 4, exdent = 4))
+}
+
