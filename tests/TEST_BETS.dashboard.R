5bf24c878aed6faf031bbbe0923ac4782a37427a
Talitha Speranza <talitha.speranza@gmail.com>
2017-01-17 18:18:46 -0200

    Arquivos inuteis retirados

---
+++ tests/TEST_BETS.dashboard.R
@@ -0,0 +1,6 @@
+parameters = list(author = "FGV/IBRE", 
+                  text = "text.txt",
+                  url = "http://portalibre.fgv.br/",
+                  logo = "logo_ibre.png")
+
+BETS.dashboard(parameters = parameters, saveas = "inst/inflation_dashboard.pdf")
