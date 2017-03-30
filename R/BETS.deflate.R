67b890815f6de59e5096050f0158d709fd477d56
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2017-03-30 19:17:28 -0300

    BETS.deflate accepts ts and deflator arguments with different frequencies if they have the same length.

--- R/BETS.deflate.R
+++ R/BETS.deflate.R
@@ -14,54 +14,76 @@
 
 BETS.deflate = function(ts, deflator, type = "index"){
   
-  freq_ts = frequency(ts)
-  freq_def = frequency(deflator)
   
-  if(freq_ts != freq_def){
-    return("ERROR")
-  }
-  
-  start_ts = start(ts)
-  start_def = start(deflator)
-  end_ts = end(ts)
-  end_def = end(deflator)
-  
-  if(start_ts[1] > start_def[1]){
-    deflator = window(deflator, start = start_ts, frequency = freq_ts)
-  } else if(start_ts[1] < start_def[1]){
-    ts = window(ts, start = start_def, frequency = freq_ts)
-  } else {
+  if(length(ts) == length(deflator)){
     
-    if(start_ts[2] > start_def[2]){
+    
+    if(type == "index"){
+      deflator = 100/deflator
+    } else if(type == "point.perc"){
+      deflator= 1/deflator
+    } else {
+      deflator = 1/(deflator/100 + 1)
+    }
+    
+    s = as.numeric(ts)
+    def = as.numeric(deflator)
+    
+    
+  } else{
+      
+      
+    freq_ts = frequency(ts)
+    freq_def = frequency(deflator)
+    
+    
+    if(freq_ts != freq_def){
+      return("ERROR")
+    }
+    
+    start_ts = start(ts)
+    start_def = start(deflator)
+    end_ts = end(ts)
+    end_def = end(deflator)
+    
+    if(start_ts[1] > start_def[1]){
       deflator = window(deflator, start = start_ts, frequency = freq_ts)
-    } else if(start_ts[2] < start_def[2]){
+    } else if(start_ts[1] < start_def[1]){
       ts = window(ts, start = start_def, frequency = freq_ts)
+    } else {
+      
+      if(start_ts[2] > start_def[2]){
+        deflator = window(deflator, start = start_ts, frequency = freq_ts)
+      } else if(start_ts[2] < start_def[2]){
+        ts = window(ts, start = start_def, frequency = freq_ts)
+      }
     }
-  }
-  
-  if(end_ts[1] > end_def[1]){
-    ts = window(ts, end = end_def, frequency = freq_ts)
-  } else if(end_ts[1] < end_def[1]){
-    deflator = window(deflator, end = end_ts, frequency = freq_ts)
-  } else {
     
-    if(end_ts[2] > end_def[2]){
+    if(end_ts[1] > end_def[1]){
       ts = window(ts, end = end_def, frequency = freq_ts)
-    } else if(end_ts[2] < end_def[2]){
+    } else if(end_ts[1] < end_def[1]){
       deflator = window(deflator, end = end_ts, frequency = freq_ts)
+    } else {
+      
+      if(end_ts[2] > end_def[2]){
+        ts = window(ts, end = end_def, frequency = freq_ts)
+      } else if(end_ts[2] < end_def[2]){
+        deflator = window(deflator, end = end_ts, frequency = freq_ts)
+      }
     }
-  }
+    
+    if(type == "index"){
+      deflator = 100/deflator
+    } else if(type == "point.perc"){
+      deflator= 1/deflator
+    } else {
+      deflator = 1/(deflator/100 + 1)
+    }
+    
+    s = as.numeric(ts)
+    def = as.numeric(deflator)
   
-  if(type == "index"){
-    deflator = 100/deflator
-  } else if(type == "point.perc"){
-    deflator= 1/deflator
-  } else {
-    deflator = 1/(deflator/100 + 1)
   }
-  
-  s = as.numeric(ts)
-  def = as.numeric(deflator)
   
   return(ts(s*def, start = start(ts), frequency = freq_ts))
 }
