3343dd56c601e4341b55c989d989d0330804a502
Talitha Speranza <talitha.speranza@gmail.com>
2017-04-07 18:17:03 -0300

    lei and cei improvments

--- R/draw.ei_comps.R
+++ R/draw.ei_comps.R
@@ -51,61 +51,116 @@ draw.ei_comps = function(){
   cols = c('rgba(38, 24, 74, 1)', 'rgba(38, 24, 74, 0.9)', 'rgba(38, 24, 74, 0.8)', 'rgba(71, 58, 131, 0.8)', 'rgba(71, 58, 134, 0.7)', 'rgba(164, 163, 204, 0.85)', 'rgba(190, 192, 213, 1)','rgba(122, 120, 168, 0.8)')
   
   p1 <- plot_ly(data.lei, type = 'bar', orientation = 'h',  width = 700, height = 450) %>%
-    add_trace(x = ~X1, name = lei.labs[1], marker = list(color = cols[1])) %>%
-    add_trace(x = ~X2, name = lei.labs[2], marker = list(color = cols[2])) %>%
-    add_trace(x = ~X3, name = lei.labs[3], marker = list(color = cols[3])) %>%
-    add_trace(x = ~X4, name = lei.labs[4], marker = list(color = cols[4])) %>%
-    add_trace(x = ~X5, name = lei.labs[5], marker = list(color = cols[5])) %>%
-    add_trace(x = ~X6, name = lei.labs[6], marker = list(color = cols[6])) %>%
-    add_trace(x = ~X7, name = lei.labs[7], marker = list(color = cols[7])) %>%
-    add_trace(x = ~X8, name = lei.labs[8], marker = list(color = cols[8])) %>%
-    layout(xaxis = list(title = "",
-                        showgrid = FALSE,
-                        showline = FALSE,
-                        showticklabels = FALSE,
-                        zeroline = T,
-                        zerolinecolor = '#969696',
-                        zerolinewidth = 3),
-           yaxis = list(showgrid = FALSE,
-                        showline = FALSE,
-                        showticklabels = FALSE,
-                        zeroline = FALSE,
-                        domain = c(0, 0.75)),
-           barmode = 'relative',
-           annotations = t,
-           #paper_bgcolor = 'rgb(248, 248, 255)', plot_bgcolor = 'rgb(248, 248, 255)',
-           showlegend = FALSE) %>%
-           add_annotations(xref = 'paper', yref = 'paper', 
-                            xanchor = 'right', x = 0, y = 0.375, 
-                            text = paste0("<b>",data.lei[1,1],"</b>"), showarrow = F,font = list(size = 16))
-  
+          layout(xaxis = list(title = "",
+                              showgrid = FALSE,
+                              showline = FALSE,
+                              showticklabels = T,
+                              zeroline = TRUE,
+                              zerolinecolor = '#969696',
+                              zerolinewidth = 3),
+                 yaxis = list(showgrid = FALSE,
+                              showline = FALSE,
+                              showticklabels = F,
+                              zeroline = FALSE,
+                              domain = c(0, 0.75)),
+                 barmode = 'relative',
+                 annotations = t,
+                 showlegend = FALSE) %>%
+                 add_annotations(xref = 'paper', yref = 'paper', 
+                                  xanchor = 'right', x = 0, y = 0.45, 
+                                  text = paste0("<b>",data.lei[1,1],"</b>"), showarrow = F,font = list(size = 18))
+      
   p2 <- plot_ly(data.cei, type = 'bar', orientation = 'h',  width = 700, height = 450) %>%
-    add_trace(x = ~X1, name = cei.labs[1], marker = list(color = cols[1])) %>%
-    add_trace(x = ~X2, name = cei.labs[2], marker = list(color = cols[2])) %>%
-    add_trace(x = ~X3, name = cei.labs[3], marker = list(color = cols[3])) %>%
-    add_trace(x = ~X4, name = cei.labs[4], marker = list(color = cols[4])) %>%
-    add_trace(x = ~X5, name = cei.labs[5], marker = list(color = cols[5])) %>%
-    add_trace(x = ~X6, name = cei.labs[6], marker = list(color = cols[6])) %>%
-    layout(xaxis = list(title = "",
-                        showgrid = FALSE,
-                        showline = FALSE,
-                        showticklabels = FALSE,
-                        zeroline = T,
-                        zerolinecolor = '#969696',
-                        zerolinewidth = 3),
-           yaxis = list(showgrid = FALSE,
-                        showline = FALSE,
-                        showticklabels = FALSE,
-                        zeroline = FALSE,
-                        domain = c(0,0.75)),
-           barmode = 'relative',
-           #paper_bgcolor = 'rgb(248, 248, 255)', plot_bgcolor = 'rgb(248, 248, 255)',
-           showlegend = FALSE) %>%
-    add_annotations(xref = 'paper', yref = 'paper', 
-                    xanchor = 'right', x = 0, y = 0.375, 
-                    text = paste0("<b>",data.cei[1,1],"</b>"), showarrow = F,font = list(size = 16))
+          layout(xaxis = list(title = "",
+                              showgrid = FALSE,
+                              showline = FALSE,
+                              showticklabels = T,
+                              zeroline = TRUE,
+                              zerolinecolor = '#969696',
+                              zerolinewidth = 3),
+                 yaxis = list(showgrid = FALSE,
+                              showline = FALSE,
+                              showticklabels = F,
+                              zeroline = FALSE,
+                              domain = c(0,0.75)),
+                 barmode = 'relative',
+                 #paper_bgcolor = 'rgb(248, 248, 255)', plot_bgcolor = 'rgb(248, 248, 255)',
+                 showlegend = FALSE) %>%
+          add_annotations(xref = 'paper', yref = 'paper', 
+                          xanchor = 'right', x = 0, y = 0.3, 
+                          text = paste0("<b>",data.cei[1,1],"</b>"), showarrow = F,font = list(size = 18))
   
-  subplot(p1,p2, nrows = 2)                                                                  
+  val.pos = 0
+  val.neg = 0
   
-  return(p)
+  for(i in 1:nrow(lei)){
+    
+    val = as.numeric(lei[i,1])
+    
+    if(abs(val) >= 0.1){
+      
+      sig = ""
+      
+      if(val > 0){
+        sig = "+"
+        pos = val.pos + val/2
+        val.pos = val.pos + val
+      } else{
+        pos = val.neg + val/2
+        val.neg = val.neg + val
+      }
+      
+      
+      p1 = p1 %>% add_trace(x = data.lei[1,i+1], name = lei.labs[1], marker = list(color = cols[i])) %>% 
+                  add_annotations(xref = 'x', yref = 'paper', 
+                                  x = pos, y = 0.45, 
+                                  text = paste0("<b>", sig, val,"%</b>"), 
+                                  showarrow = F,font = list(size = 16, color = "#FFFFFF")) %>%
+        add_annotations(xref = 'x', yref = 'paper', 
+                        x = pos, y = 1, 
+                        text = lei[i,2], 
+                        showarrow = F,font = list(size = 14))
+    }
+  }
+  
+  fac = sum(abs(lei[,1]))/sum(abs(cei[,1]))
+  
+  val.pos = 0
+  val.neg = 0
+  
+  for(i in 1:nrow(cei)){
+    
+    val = as.numeric(cei[i,1])
+    
+    if(abs(val) > 0.03){
+      
+      sig = ""
+      
+      if(val > 0){
+        sig = "+"
+        pos = fac*(val.pos + val/2)
+        val.pos = val.pos + val
+      } else{
+        pos = fac*(val.neg + val/2)
+        val.neg = val.neg + val
+      }
+      
+      print(val)
+      print(pos)
+      
+      p2 = p2 %>% add_trace(x = data.cei[1,i+1], name = cei.labs[1], marker = list(color = cols[i])) %>% 
+        add_annotations(xref = 'x', yref = 'paper', 
+                        x = pos, y = 0.3, 
+                        text = paste0("<b>", sig, val,"%</b>"), 
+                        showarrow = F,font = list(size = 16, color = "#FFFFFF")) %>%
+        add_annotations(xref = 'x', yref = 'paper', 
+                        x = pos, y = 0.85, 
+                        text = cei[i,2], 
+                        showarrow = F,font = list(size = 14))
+    }
+  }
+  
+  s = subplot(p1,p2, nrows = 2)                                                                  
+  
+  return(s)
 }
