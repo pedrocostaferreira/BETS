fdb57627ad53603af2a2d568072ce0c0c66ff18f
talithafs <talitha.speranza@gmail.com>
2017-05-06 22:28:43 -0300

    Re-adding situation charts

---
+++ R/draw.cdb.R
@@ -0,0 +1,42 @@
+#' @title Create a chart of the Time Deposits time series 
+#' 
+#' @description  Creates a plot of series 14
+#' 
+#' @param start A \code{character}. The stating period of the series.
+#' @param ylim A \code{numeric vector}. Y axis limits.
+#' @param xlim  A \code{numeric vector}. x axis limits.
+#' 
+#' @importFrom grDevices dev.new dev.off  pdf  png
+#' @importFrom utils read.csv2
+#' @importFrom stats ts plot.ts
+#' @importFrom graphics axis text  points  mtext arrows
+#' 
+#' @return An image file is saved in the 'graphs' folder, under the BETS installation directory. 
+
+draw.cdb= function(start = NULL, ylim = NULL, xlim = NULL){
+
+  cdb = BETS.get(code = 14, data.frame = TRUE)
+  
+  if(!is.null(start)){
+    
+    if(start[2] < 9){
+      start[2] = paste0("0",start[2])
+    }
+    if(start[3] < 9){
+      start[3] = paste0("0",start[3])
+    }
+    
+    init = as.Date(paste0(start[1],"-",start[2],"-",start[3]), format = "%Y-%m-%d")
+    
+    i = which(cdb[,"date"] >= init)
+    
+    if(length(i) != 0){
+      cdb = cdb[i,]
+    }
+  }
+  
+  lims = chart.add_basic(ts = cdb, ylim = ylim, xlim = xlim, title = "Time Deposits (CDB/RDB-Preset)", subtitle = "Daily Returns (%)", col = "darkolivegreen", leg.pos = "bottom")
+  chart.add_notes(ts(cdb[,"value"], frequency = 365), ylim = lims[3:4], xlim = lims[1:2],dec = 4)
+  
+  
+}
