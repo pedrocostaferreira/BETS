59a723749cba3bf9e10cdf3548de3a46a6d41f56
talithafs <talitha.speranza@gmail.com>
2017-05-06 23:08:17 -0300

    Re-adding conjucture charts and files

--- R/BETS.chart.R
+++ R/BETS.chart.R
@@ -5,6 +5,7 @@
 #' @note It is not yet possible to make charts of custom series. 
 #' 
 #' @param ts A \code{character} or a \code{ts} object. A custom time series or the name of a pre-defined series. A complete list of names is under the 'Details' section.
+#' @param style A \code{character}. Should the chart be made with Plotly (style = "plotly") or with R standard library (style = "normal")?
 #' @param lang A \code{character}. The language. For now, only 'en' (english) is available.
 #' @param file A \code{character}. The whole path, including a custom name, for the output (an image file). The default value is NULL. If left to NULL, the chart will be rendered in the standard R plotting area.
 #' @param open  A \code{boolean}. Whether to open the file containing the chart.
@@ -13,6 +14,8 @@
 #' @details 
 #' 
 #' \bold{Names of pre-defined charts:}
+#' 
+#' Business Cycle Dashboard:
 #' 
 #' \tabular{lll}{
 #'  VALUE \tab DESCRIPTION \tab CODE \cr
@@ -33,6 +36,20 @@
 #'  \bold{'retail'} \tab Retail Sellers Confidence Index (Expectations versus Present Situation) \tab (*) \cr
 #'  \bold{'consm'} \tab Consumer Confidence Index (Expectations versus Present Situation) \tab (*) 
 #'}
+#'
+#' Conjucture Dashboard:
+#' 
+#' #' \tabular{lll}{
+#'  VALUE \tab DESCRIPTION \tab CODE \cr
+#'  \bold{'ipca_with_core'} \tab National consumer price index (IPCA) - in 12 months and  Broad national consumer price index - Core IPCA trimmed means smoothed \tab 13522 and 4466 \cr
+#'  \bold{'ulc'} \tab Unit labor cost - ULC-US$ - June/1994=100 \tab 11777 \cr
+#'  \bold{'eap'} \tab Economically active population \tab 10810 \cr
+#'  \bold{'cdb'} \tab Time deposits (CDB/RDB-preset) - Daily return (percentage) \tab 14 \cr
+#'  \bold{'indprod'} \tab Prodcution Indicators (2012=100) - General	\tab 21859 \cr
+#'  \bold{'selic'} \tab Interest rate - Selic accumulated in the month in annual terms (basis 252) \tab 4189 \cr
+#'  \bold{'unemp'} \tab Open unemployment rate - by metropolitan region - Brasil (weekly) \tab 10777\cr
+#'  \bold{'vargdp'} \tab GDP - real percentage change in the year \tab 7326 
+#'}
 #' 
 #' (*) Not in BETS databases yet. But you can find it in .csv files saved under your BETS installation directory.
 #' 
@@ -52,7 +69,7 @@
 #' @export
 
 
-BETS.chart = function(ts, file = NULL, open = TRUE, lang = "en", params = NULL){
+BETS.chart = function(ts, style = "normal", file = NULL, open = TRUE, lang = "en", params = NULL){
   
   if(lang == "en"){
     Sys.setlocale(category = "LC_ALL", locale = "English_United States.1252")
@@ -101,11 +118,43 @@ BETS.chart = function(ts, file = NULL, open = TRUE, lang = "en", params = NULL){
     } else if(ts %in% c("transf_ind","servc","retail","constr","consm")){
       p = draw.survey(ts)
     } else {
-      msg(paste("Plot was not created.",.MSG_PARAMETER_NOT_VALID))
+      
+      if(!is.null(file)){
+        if(grepl("\\.png", file)){
+          png(file,width=728,height=478, pointsize = 15) 
+        }
+        else {
+          pdf(file, width = 7, height = 4.5)
+        } 
+      }
+      
+      if(ts == "ipca_with_core"){
+        draw.ipca()
+      } else if(ts == "ulc"){
+        draw.ulc()
+      } else if(ts == "eap"){
+        draw.eap()
+      } else if(ts == "cdb"){
+        draw.cdb()
+      } else if(ts == "indprod"){
+        draw.indprod()
+      } else if(ts == "selic"){
+        draw.selic()
+      } else if(ts == "unemp"){
+        draw.unemp()
+      } else if(ts == "vargdp"){
+        draw.vargdp()
+      } else {
+        msg(paste("Plot was not created.",.MSG_PARAMETER_NOT_VALID))
+      }
+      
+      if(!is.null(file)){
+        dev.off()
+      }
     }
     
   } else {
-    #p = draw.generic(ts, params)
+    #p = draw.generic(ts, params, type)
   }
   
   if(!is.null(file)){
@@ -120,6 +169,8 @@ BETS.chart = function(ts, file = NULL, open = TRUE, lang = "en", params = NULL){
     if(open){
       file.show(file)
     }
+    
+    
   }
   else {
     p 
--- tests/TEST_BETS.chart.R
+++ tests/TEST_BETS.chart.R
@@ -55,3 +55,32 @@ BETS.chart(ts = "lab_mrkt")
 BETS.chart(ts = "cap_utl", file = "graphs/cap_utl.png", open = T)
 BETS.chart(ts = "cap_utl", file = "graphs/cap_utl", open = F)
 BETS.chart(ts = "cap_utl")
+
+## Conjuncture Dashboard
+
+BETS.chart("ipca_with_core", file = "ipca_with_core.png", open = F)
+BETS.chart("ipca_with_core", out = "pdf", open = F)
+
+BETS.chart("ulc", out = "png", open = F)
+BETS.chart("ulc", out = "pdf", open = F)
+
+BETS.chart("unemp", out = "pdf", open = F)
+BETS.chart("unemp", out = "png", open = F)
+
+BETS.chart("vargdp", out = "pdf", open = F)
+BETS.chart("vargdp", out = "png", open = F)
+
+BETS.chart("indprod", out = "pdf", open = F)
+BETS.chart("indprod", out = "png", open = F)
+
+BETS.chart("eap", out = "pdf", open = F)
+BETS.chart("eap", out = "png", open = F)
+
+BETS.chart("cdb", out = "pdf", open = F)
+BETS.chart("cdb", out = "png", open = F)
+
+BETS.chart("selic", out = "pdf", open = F)
+BETS.chart("selic", out = "png", open = F)
+
+
+BETS.chart("ipca_with_core", out = "pdf", open = T)
