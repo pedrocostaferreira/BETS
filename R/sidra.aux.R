311ef746f1749fa587022182496e68f07e70e2df
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2017-06-01 19:07:05 -0300

    Dealing with big monthly data.

--- R/sidra.aux.R
+++ R/sidra.aux.R
@@ -3,7 +3,7 @@
 #' Searches the Sidra databases for a series by its description or a given table descriptions.
 #'
 #' @param x Either a character or a numeric. If character, function searches the Sidra metadata. If a numeric argument is provided the descriptions of the given table are seached .
-#' @import xml2 rvest stringr
+#' @import xml2 rvest stringr lubridate
 
 
 sidra.aux <- function(x, len, nova_req) {
@@ -21,7 +21,7 @@ sidra.aux <- function(x, len, nova_req) {
     
     
     
-    if (d1 = stringr::str_count(d, "Ano") == 1){
+    if (stringr::str_count(d, "Ano") == 1){
         
         
         minus = to - from
@@ -53,19 +53,77 @@ sidra.aux <- function(x, len, nova_req) {
             
         }
         
-    } else if (d1 = stringr::str_count(d, "Mês") == 1){
-        
-        minus = to - from
-        minus = floor(minus/ 3)
+        colnames(tabela) <- unlist(header2)
         
         
         
+    } else if (stringr::str_count(d, "Mês") == 1){
+        
+       
+        
+        # month(to2) <- month(to2) - month(from2)
+    
         for(i in len){
             
             tabela = data.frame()
             header2 = NULL
             
-            for(j in seq()){        
+            
+            from2 = paste0(substr(from,1,4), "-", substr(from,5,6), "-01")
+            to2 = paste0(substr(to,1,4), "-", substr(to,5,6), "-01")
+            from2 = as.Date(from2)
+            to2 = as.Date(to2)
+            dif_mes = as.numeric(floor((to2 - from2)/365*12/nova_req))
+            month(from2) = dif_mes + month(from2) 
+            
+            init = paste0(substr(from2,1,4),substr(from2,6,7))
+            fin = paste0(substr(from2,1,4),substr(from2,6,7)) 
+            
+            
+            for(j in 1:nova_req){        
+        
+                tabela1=RCurl::getURL(paste0("http://api.sidra.ibge.gov.br/values/",
+                                    "t/", inputs[i], "/", territory, "/", "p/", 
+                                    init, "-", fin,  
+                                    "/v/", variable[i], "/f/", "u", "/h/", header,
+                                    sections[[i]]),
+                             ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
+                
+                
+                
+                init = paste0(sum(as.numeric(substr(from2,1,4)),1),substr(from2,6,7))
+                month(from2) = dif_mes + month(from2)
+                fin = paste0(substr(from2,1,4),substr(from2,6,7)) 
+                
+                t1 = paste("tabela", x, sep="_")
+                tabela1 = rjson::fromJSON(tabela1)
+                tabela1 = as.data.frame(do.call("rbind", tabela1))
+                header2 = tabela1[1,]
+                tabela = as.data.frame(do.call("rbind", list(tabela, tabela1[2:nrow(tabela1),])))
+                
+                
+            }
+            
+        }
+        
+        colnames(tabela) <- unlist(header2)
+        
+        
+        
+    } else if(stringr::str_count(d, "Trimestre") == 1){
+        
+        
+        for(i in len){
+            
+            
+            
+            
+            
+            tabela = data.frame()
+            header2 = NULL
+            
+            
+            for(j in 1:nova_req){
         
                 tabela=RCurl::getURL(paste0("http://api.sidra.ibge.gov.br/values/",
                                     "t/", inputs[i], "/", territory, "/", "p/", 
@@ -73,23 +131,11 @@ sidra.aux <- function(x, len, nova_req) {
                                     "/v/", variable[i], "/f/", "u", "/h/", header,
                                     sections[[i]]),
                              ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
-                
+            
+            
             }
             
         }
-        
-
-        
-    } else if(d1 = stringr::str_count(d, "Trimestre") == 1){
-        
-        
-        tabela=RCurl::getURL(paste0("http://api.sidra.ibge.gov.br/values/",
-                                    "t/", inputs[i], "/", territory, "/", "p/", 
-                                    from, "-", to,  
-                                    "/v/", variable[i], "/f/", "u", "/h/", header,
-                                    sections[[i]]),
-                             ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
-        
 
         
     }
@@ -97,6 +143,73 @@ sidra.aux <- function(x, len, nova_req) {
     
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-    return(writeLines(d3))
+    if ( colnames(tabela[id3]) == "M\u00EAs" & length(tabela[[id3]]) > 1){
+        
+        tabela$mes <- sapply(tabela["M\u00EAs"], 
+                             FUN = function(x){substr(x,1,(nchar(x)-5))}) 
+        tabela$ano <- sapply(tabela["M\u00EAs"], 
+                             FUN = function(x){substr(x,(nchar(x)-3), nchar(x))}) 
+        
+        
+        tabela$mes[tabela$mes == "janeiro"] <- "01"
+        tabela$mes[tabela$mes == "fevereiro"] <- "02"
+        tabela$mes[tabela$mes == "mar\u00E7o"] <- "03"
+        tabela$mes[tabela$mes == "abril"] <- "04"
+        tabela$mes[tabela$mes == "maio"] <- "05"
+        tabela$mes[tabela$mes == "junho"] <- "06"
+        tabela$mes[tabela$mes == "julho"] <- "07"
+        tabela$mes[tabela$mes == "agosto"] <- "08"
+        tabela$mes[tabela$mes == "setembro"] <- "09"
+        tabela$mes[tabela$mes == "outubro"] <- "10"
+        tabela$mes[tabela$mes == "novembro"] <- "11"
+        tabela$mes[tabela$mes == "dezembro"] <- "12"
+        
+        tabela$mes_ano <- base::paste0(tabela$ano, "-",tabela$mes, "-01")
+        tabela$mes_ano <- base::as.Date(tabela$mes_ano)
+        tabela["M\u00EAs"] <- tabela$mes_ano
+        tabela <- tabela[,1:(length(tabela)-3)]
+        colnames(tabela)[id3] <- "Data"
+        
+    }
+    
+    if(colnames(tabela[id3]) == "Ano" & length(tabela[[id3]]) > 1){ 
+        tabela$Ano <- base::paste0(tabela$Ano, "-01-01")
+        tabela$Ano <- base::as.Date(tabela$Ano)
+        colnames(tabela)[id3] <- "Data"
+        
+    }
+    
+    if(colnames(tabela[id3]) == "Trimestre" & length(tabela[[id3]]) > 1){
+        
+        tabela$trimestre <- sapply(tabela["Trimestre"], 
+                                   FUN = function(x){substr(x,1,1)}) 
+        tabela$ano <- sapply(tabela["Trimestre"], 
+                             FUN = function(x){substr(x,(nchar(x)-3), nchar(x))}) 
+        
+        
+        tabela$tri_ano <- base::paste0(tabela$ano, "-0",tabela$trimestre)
+        tabela$tri_ano <- zoo::as.yearqtr(tabela$tri_ano)
+        tabela["Trimestre"] <- tabela$tri_ano
+        tabela <- tabela[,1:(length(tabela)-3)]
+        colnames(tabela)[id3] <- "Data"    
+    }
+    
+    #Transformando a coluna V em valor
+    
+    valor = NULL
+    id = which(colnames(tabela)=="V" | colnames(tabela)=="Valor")
+    tabela[,id] = suppressWarnings(ifelse(unlist(tabela[,id])!="..", 
+                                          as.numeric(unlist(tabela[,id])),NA))
+    rm(tabela2)
 }
