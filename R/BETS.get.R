2c776e847693ac38fcb3a00593e2231d2450352b
jonatha azevedo <johnmasterchip@gmail.com>
2017-07-13 17:39:09 -0300

    Add connection function

--- R/BETS.get.R
+++ R/BETS.get.R
@@ -97,7 +97,7 @@ BETS.get = function(code, from = "", to = "", data.frame = FALSE, frequency = NU
   } else {
     
     freq = 365 
-    conn = dbConnect(MySQL(),db="bets",user="BETS_user",password="123456",host="200.20.164.178",port=3306)
+    conn = connection()
     
     aux = dbGetQuery(conn,paste0("select date, value from bets.IPC where code = '",code,"'"))
     
